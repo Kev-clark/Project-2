45597dfce52229e59f99f2506ee3842388194f47
kevin.clark@ctx.edu <kevin.uzziel.clark@gmail.com>
2019-12-01 18:30:45 -0600

    fixed app.py

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,27 @@
+
+
+function init() {
+    // Grab a reference to the dropdown select element
+    var selector = d3.select("#selDataset");
+  
+    // Use the list of sample names to populate the select options
+    d3.json("/games").then((sampleNames) => {
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+  
+      // Use the first sample from the list to build the initial plots
+      const firstSample = sampleNames[0];
+      console.log("init"+firstSample);
+      getData(firstSample);
+      
+     
+    });
+  }
+
 var LeafIcon = L.Icon.extend({
   options: {
       shadowUrl: 'leaf-shadow.png',
@@ -9,17 +33,25 @@ var LeafIcon = L.Icon.extend({
   }
 });
 
-var goldMedalIcon = new LeafIcon({iconUrl: 'gold.png'}),
-    silverMedalIcon = new LeafIcon({iconUrl: 'Silver.png'}),
-    BronzeMedalIcon = new LeafIcon({iconUrl: 'Bronze.png'});
+var goldMedalIcon = new LeafIcon({iconUrl: '../Gold.png'}),
+    silverMedalIcon = new LeafIcon({iconUrl: '../Silver.png'}),
+    BronzeMedalIcon = new LeafIcon({iconUrl: '../Bronze.png'});
 
-function buildMap(data)
+async function getData(sample){
+//Clear Current map HTML to reset:
+;
 
+  // @TODO: Use `d3.json` to fetch the sample data for the plots
+  const data= await d3.json('/medals/'+sample).then(data=>buildMap(data))
+}
+
+function buildMap(countries){
+    console.log(countries)
     // Create a map object
-const myMap = L.map("map", {
-    center: [0, 0],
-    zoom: 3
-});
+var myMap = L.map("map", {
+        center: [0, 0],
+        zoom: 3
+    });
 
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
@@ -28,7 +60,7 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: API_KEY
 }).addTo(myMap);
 
-const countries = data;
+
 // Loop through the cities array and create one marker for each city object
 countries.forEach(country => {
     var location=[country.lat, country.long];
@@ -59,7 +91,7 @@ countries.forEach(country => {
            }).bindPopup("<h1>" + country.Name + "</h1> <hr> <br>Total events competed: "+country.count+"<h3>Medals:  <br>Gold: "+country.gold+"<br>Silver: "+country.silver+"<br>Bronze: "+country.bronze+"</h3>").addTo(myMap);
     }
     else {
-        color = "red";
+        color = "black";
         L.circle(location, {
             fillOpacity: 0.75,
             color: "white",
@@ -71,3 +103,17 @@ countries.forEach(country => {
     }
 
 })
+}
+function optionChanged(newSample) {
+    const mapSelector = d3.select("#map");
+    mapSelector.html("")
+    // Fetch new data each time a new sample is selected
+    getData(newSample);
+      }
+      
+init();
+// needs added to HTML : this is where the list of the
+// different olymics will append to and will call the new map when new value is selected.
+//Must not be nested in map html or it will be wiped.
+
+//<select id="selDataset" onchange="optionChanged(this.value)"></select>
