39b6f75d49c1ea0cdedf7adfe12524e9cd9e454e
kevin.clark@ctx.edu <kevin.uzziel.clark@gmail.com>
2019-12-02 11:38:58 -0600

    final js

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,5 +1,6 @@
 
 
+
 function init() {
     // Grab a reference to the dropdown select element
     var selector = d3.select("#selDataset");
@@ -15,12 +16,13 @@ function init() {
   
       // Use the first sample from the list to build the initial plots
       const firstSample = sampleNames[0];
-      console.log("init"+firstSample);
+      console.log(firstSample);
       getData(firstSample);
       
      
     });
   }
+  init();
 
 var LeafIcon = L.Icon.extend({
   options: {
@@ -39,7 +41,6 @@ var goldMedalIcon = new LeafIcon({iconUrl: 'https://github.com/Kev-clark/Project
 
 async function getData(sample){
 //Clear Current map HTML to reset:
-;
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
   const data= await d3.json('/medals/'+sample).then(data=>buildMap(data))
@@ -48,21 +49,8 @@ async function getData(sample){
 function buildMap(countries){
     console.log(countries)
     // Create a map object
-var myMap = L.map("map", {
-        center: [0, 0],
-        zoom: 3
-    });
-
-L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.streets-basic",
-    accessToken: API_KEY
-}).addTo(myMap);
-
-
 // Loop through the cities array and create one marker for each city object
-countries.forEach(country => {
+const countryMarkers = countries.map(country => {
     var location=[country.lat, country.long];
         if(country.long == null || country.lat == null){
             location=[25,-50]
@@ -71,47 +59,78 @@ countries.forEach(country => {
     let color = "";
     if (country.gold > country.silver &&country.gold > country.bronze) {
         color = "gold";
-        L.marker(location, {
+        return L.marker(location, {
             icon: goldMedalIcon
             
-           }).bindPopup("<h1>" + country.Name + "</h1> <hr> <br>Total events competed: "+country.count+"<h3>Medals:  <br>Gold: "+country.gold+"<br>Silver: "+country.silver+"<br>Bronze: "+country.bronze+"</h3>").addTo(myMap);
+           }).bindPopup("<h1>" + country.Name + "</h1> <hr> <br>Total Competitions Entered: "+country.count+"<h3>Medals:  <br>Gold: "+country.gold+"<br>Silver: "+country.silver+"<br>Bronze: "+country.bronze+"</h3>");
     }
     else if (country.silver > country.bronze) {
         color = "silver";
-        L.marker(location, {
+        return L.marker(location, {
             icon: silverMedalIcon
             
-           }).bindPopup("<h1>" + country.Name + "</h1> <hr> <br>Total events competed: "+country.count+"<h3>Medals:  <br>Gold: "+country.gold+"<br>Silver: "+country.silver+"<br>Bronze: "+country.bronze+"</h3>").addTo(myMap);
+           }).bindPopup("<h1>" + country.Name + "</h1> <hr> <br>Total Competitions Entered: "+country.count+"<h3>Medals:  <br>Gold: "+country.gold+"<br>Silver: "+country.silver+"<br>Bronze: "+country.bronze+"</h3>");
     }
     else if (country.bronze > 0) {
         color = "bronze";
-        L.marker(location, {
+        return L.marker(location, {
             icon:  BronzeMedalIcon
             
-           }).bindPopup("<h1>" + country.Name + "</h1> <hr> <br>Total events competed: "+country.count+"<h3>Medals:  <br>Gold: "+country.gold+"<br>Silver: "+country.silver+"<br>Bronze: "+country.bronze+"</h3>").addTo(myMap);
+           }).bindPopup("<h1>" + country.Name + "</h1> <hr> <br>Total Competitions Entered: "+country.count+"<h3>Medals:  <br>Gold: "+country.gold+"<br>Silver: "+country.silver+"<br>Bronze: "+country.bronze+"</h3>");
     }
     else {
         color = "black";
-        L.circle(location, {
+        return L.circle(location, {
             fillOpacity: 0.75,
             color: "white",
             fillColor: color,
             // Adjust radius
-            radius: country.count * 1500
+            radius: country.count * 3000
             
-           }).bindPopup("<h1>" + country.Name + "</h1> <hr> <br>Total events competed: "+country.count+"<h3>Medals:  <br>Gold: "+country.gold+"<br>Silver: "+country.silver+"<br>Bronze: "+country.bronze+"</h3>").addTo(myMap);
+           }).bindPopup("<h1>" + country.Name + "</h1> <hr> <br>Total Competitions Entered: "+country.count+"<h3>Medals:  <br>Gold: "+country.gold+"<br>Silver: "+country.silver+"<br>Bronze: "+country.bronze+"</h3>");
     }
 
 })
+console.log(countryMarkers);
+const cityLayer = L.layerGroup(countryMarkers);
+const mainMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+maxZoom: 18,
+id: "mapbox.emerald",
+accessToken: API_KEY
+});
+const baseMaps = {
+   main: mainMap
+    
+};
+
+// Overlays that may be toggled on or off
+const overlayMaps = {
+    Medals: cityLayer
+};
+var myMap = L.map("map", {
+    center: [0, 0],
+    zoom: 3,
+    layers:[mainMap, cityLayer]
+});
+
+L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+
 }
+
+function removeLayers(){
+    d3.selectAll("div").node().remove()
+    d3.select("body").append("div").attr("id", "map");
+};
+
 function optionChanged(newSample) {
-    const mapSelector = d3.select("#map");
-    mapSelector.html("")
+    
+    removeLayers();
     // Fetch new data each time a new sample is selected
     getData(newSample);
-      }
+      };
       
-init();
+
 // needs added to HTML : this is where the list of the
 // different olymics will append to and will call the new map when new value is selected.
 //Must not be nested in map html or it will be wiped.
